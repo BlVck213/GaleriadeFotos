546d0a9b306ac7ea9c075a3ac7e2ad7c8405bf3b
BlVck213 <paulocesarrrpontes@gmail.com>
2021-10-05 10:17:07 -0300

    Galeria Finalizada

--- galeria.js
+++ galeria.js
@@ -1,45 +1,61 @@
 "use strict"
 
-const imagens = [
-
-    "./img/future.jpg",
-    "./img/future1.jpg",
-    "./img/future2.jpg",
-    "./img/future3.jpg",
-    "./img/future4.jpg",
-    "./img/future5.jpg",
-    "./img/future6.jpg",
-    "./img/future7.jpg"
-
-]
+const limpar = (elemento) => {
+    while (elemento.firstChild) {
+        elemento.removeChild(elemento.lastChild)
+    }
+}
 
 
-const limparId = (urlImg) => urlImg.split("/")[2].split(".")[0].replace(" ","-").toLowerCase()
+const pegarImagens = (raca) => fetch(`https://dog.ceo/api/breed/${raca}/images`)
 
+const procurarImagens = async (evento) => {
 
-const criarItem = (urlImg) => {
+    if (evento.key === 'Enter'){
+        const raca = evento.target.value
+        const imagesResponse = await pegarImagens(raca)
+        const imagens = await imagesResponse.json()
+  
+        limpar(document.querySelector(".galeria-container"))
+        limpar(document.querySelector(".slide-container"))
+        carregarImagens(imagens.message)
+        carregarSlides(imagens.message)
+    }
+
+}
+
+const limparId = (urlImagem) => {
+    const posBarra = urlImagem.lastIndexOf('/') + 1
+    const posPonto = urlImagem.lastIndexOf('.') 
+    return urlImagem.substring(posBarra, posPonto)
+}
+
+const criarItem = (urlImagem) => {
     const container = document.querySelector(".galeria-container")
 
     const novoLink = document.createElement("a")
-    novoLink.href = `#${limparId(urlImg)}`
+    novoLink.href = `#${limparId(urlImagem)}`
     novoLink.classList.add("galeria-itens")
-    novoLink.innerHTML = `<img src="${urlImg}" alt="">`
+    novoLink.innerHTML = `<img src="${urlImagem}" alt="">`
 
     container.appendChild(novoLink)
             
 }
 
-const criarSlide = (urlImg) => {
+const carregarImagens = (imagens) => imagens.forEach(criarItem)
+
+const criarSlide = (urlImagem, indice, arr) => {
     const container = document.querySelector(".slide-container")
 
     const novoSlide = document.createElement("div")
     novoSlide.classList.add("slide")
-    novoSlide.id = limparId(urlImg)
+    novoSlide.id = limparId(urlImagem)
 
-   const indiceAnterior = indice > 0 ? indice -1 : arr.lenght -1
+
+   const indiceAnterior = indice > 0 ? indice - 1 : arr.length - 1
     const idAnterior = limparId(arr[indiceAnterior])
 
-    const indiceProximo = indice < arr.lenght - 1 ? indice + 1 : 0 
+    const indiceProximo = indice < arr.length - 1 ? indice + 1 : 0 
     const idProximo = limparId(arr[indiceProximo])
 
    
@@ -47,16 +63,15 @@ const criarSlide = (urlImg) => {
         `<div class="image-container">
             <a href="#" class="fechar">&#10006;</a>
             <a href="#${idAnterior}" class="navegacao anterior">&#171;</a>
-            <a href="${idProximo}" class="navegacao proximo">&#187;</a>
-            <img src="#${urlImg}" alt="">
+            <a href="#${idProximo}" class="navegacao proximo">&#187;</a>
+            <img src="${urlImagem}" alt="">
         </div>`
 
     container.appendChild(novoSlide)
 }
 
 
-const carregarSlides = () => imagens.forEach(criarSlide)
-const carregarImagens = () => imagens.forEach(criarItem)
+const carregarSlides = (imagens) => imagens.forEach(criarSlide)
 
-carregarImagens()
-carregarSlides()
+
+document.querySelector(".pesquisa input").addEventListener("keypress", procurarImagens)
